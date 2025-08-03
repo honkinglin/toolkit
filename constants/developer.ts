1e51b81610da891f023319060ed90c7951b2628c
Kyrie Lin <kyrielin66@gmail.com>
2025-08-03 10:20:07 -0700

    feat: sidebar

--- constants/developer.ts
+++ constants/developer.ts
@@ -1,19 +1,4 @@
-import {
-  GitBranch,
-  Terminal,
-  Clock,
-  Braces,
-  Archive,
-  FileSpreadsheet,
-  Database,
-  Calculator,
-  Package,
-  Code,
-  FileText,
-  Mail,
-  Type,
-  BookOpen,
-} from 'lucide-react';
+import { Clock, Braces, Archive, FileSpreadsheet, Database, Calculator, Code } from 'lucide-react';
 
 export const DEVELOPER_ROUTES = {
   CRONTAB_EXPRESSION_GENERATOR: '/crontab-expression-generator',
@@ -22,12 +7,6 @@ export const DEVELOPER_ROUTES = {
   JSON_TO_CSV: '/json-to-csv',
   SQL_BEAUTIFY_FORMAT: '/sql-beautify-format',
   CHMOD_CALCULATOR: '/chmod-calculator',
-  DOCKER_RUN_TO_DOCKER_COMPOSE: '/docker-run-to-docker-compose',
-  XML_FORMAT: '/xml-format',
-  YAML_BEAUTIFY_FORMAT: '/yaml-beautify-format',
-  EMAIL_NORMALIZER: '/email-normalizer',
-  REGEX_TESTER: '/regex-tester',
-  REGEX_CHEATSHEET: '/regex-cheatsheet',
 };
 
 export const developerNavigationConfig = {
@@ -64,36 +43,6 @@ export const developerNavigationConfig = {
       titleKey: 'chmodCalculator',
       url: DEVELOPER_ROUTES.CHMOD_CALCULATOR,
       icon: Calculator,
-    },
-    {
-      titleKey: 'dockerRunToDockerCompose',
-      url: DEVELOPER_ROUTES.DOCKER_RUN_TO_DOCKER_COMPOSE,
-      icon: Package,
-    },
-    {
-      titleKey: 'xmlFormat',
-      url: DEVELOPER_ROUTES.XML_FORMAT,
-      icon: Code,
-    },
-    {
-      titleKey: 'yamlBeautifyFormat',
-      url: DEVELOPER_ROUTES.YAML_BEAUTIFY_FORMAT,
-      icon: FileText,
-    },
-    {
-      titleKey: 'emailNormalizer',
-      url: DEVELOPER_ROUTES.EMAIL_NORMALIZER,
-      icon: Mail,
-    },
-    {
-      titleKey: 'regexTester',
-      url: DEVELOPER_ROUTES.REGEX_TESTER,
-      icon: Type,
-    },
-    {
-      titleKey: 'regexCheatsheet',
-      url: DEVELOPER_ROUTES.REGEX_CHEATSHEET,
-      icon: BookOpen,
     },
   ],
 };
