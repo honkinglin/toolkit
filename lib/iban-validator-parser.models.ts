35577e1ec8522c12d17c2d512123804d8abdf006
Kyrie Lin <kyrielin66@gmail.com>
2025-08-03 22:14:02 -0700

    feat: iban

---
+++ lib/iban-validator-parser.models.ts
@@ -0,0 +1,23 @@
+import { ValidationErrorsIBAN } from 'ibantools';
+
+const ibanErrorToMessage = {
+  [ValidationErrorsIBAN.NoIBANProvided]: 'No IBAN provided',
+  [ValidationErrorsIBAN.NoIBANCountry]: 'No IBAN country',
+  [ValidationErrorsIBAN.WrongBBANLength]: 'Wrong BBAN length',
+  [ValidationErrorsIBAN.WrongBBANFormat]: 'Wrong BBAN format',
+  [ValidationErrorsIBAN.ChecksumNotNumber]: 'Checksum is not a number',
+  [ValidationErrorsIBAN.WrongIBANChecksum]: 'Wrong IBAN checksum',
+  [ValidationErrorsIBAN.WrongAccountBankBranchChecksum]: 'Wrong account bank branch checksum',
+  [ValidationErrorsIBAN.QRIBANNotAllowed]: 'QR-IBAN not allowed',
+};
+
+export function getFriendlyErrors(errorCodes: ValidationErrorsIBAN[]): string[] {
+  return errorCodes.map((errorCode) => ibanErrorToMessage[errorCode]).filter(Boolean);
+}
+
+export interface IbanDetail {
+  label: string;
+  value: string | boolean | string[] | undefined;
+  showCopyButton?: boolean;
+  hideOnNil?: boolean;
+}
